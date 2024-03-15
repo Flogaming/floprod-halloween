e73f99fff3bfb5e15ea886688bb1e16d962f23ee
Flogaming <49497412+Flogaming@users.noreply.github.com>
2024-03-15 19:02:32 +0100

    Nouveau statut

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["C'est terminé Halloween ?"];
 
 
 let currentIndex = 0;
